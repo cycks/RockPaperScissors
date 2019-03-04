c79c643ebc855277ffc1cc2c3822c399a7334067
sikolia <sikolia.wycliffe@gmail.com>
2019-03-04 19:08:18 +0300

    Edit main.js to refactor getInput function

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-    let singleRound = function (playerSelection, computerSelection) {
+    let PlaySingleRound = function (playerSelection, computerSelection) {
         var playerSelection = playerSelection.toLowerCase();
         let playerWins = "Player Wins! You selected " + playerSelection + " I selected " + computerSelection;
         let computerWins = "I win! I selected " + computerSelection + " you selected " + playerSelection;
@@ -39,28 +39,38 @@
     let computerScore = 0;
     let rounds = 0;
 
-    var getInput = function(){                
-        let playerSelection = event.srcElement.id
-        let possibleChoices = ["rock", "paper", "scissors"];
-        let computerSelection = possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
-        let roundScore = singleRound(playerSelection, computerSelection);
+    let findRoundWinner = function(playerScore, computerScore){
+        if (playerScore > computerScore) {
+            var final = "Player wins with a score of " + playerScore + " against computer " + computerScore
+        } else if (playerScore > computerScore){
+            var final = "Computer wins with a score of " + computerScore + " against player " + playerScore
+        } else{
+            var final = "We have a tie. We both scored " + playerScore
+        }
+        return final
+    }
 
-        if (roundScore.startsWith("Player")) {
-                // alert("Player wins")
+    let increaseScore = function(roundScore){
+        if (roundScore.startsWith("Player")) {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
                 playerScore += 1
             } else if (roundScore.startsWith("I win!")){
                 computerScore += 1
             }else{
                 // Python's equivalent of pass
             }
-        
-        if (playerScore > computerScore) {
-            var final = "Player wins with a score of " + playerScore + " against computer " + computerScore
-        } else {
-            var final = "Computer wins with a score of " + computerScore + " against player " + playerScore
-        } 
-        rounds += 1
-        if (rounds == 5){
+    }
 
-        }
+
+    let getInput = function(){                
+        let playerSelection = event.srcElement.id
+        let possibleChoices = ["rock", "paper", "scissors"];
+        let computerSelection = possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
+        let roundScore = PlaySingleRound(playerSelection, computerSelection);
+        increaseScore(roundScore)
+        
+        
+        rounds += 1
+        // if (rounds == 5){
+
+        // }
     }
